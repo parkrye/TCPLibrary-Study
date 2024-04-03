f91c5754978748d66a48780a70a96fa105e4af8b
parkrye <parkrye@gmail.com>
2024-04-03 10:49:12 +0900

    초기 커밋.

---
+++ TCPLibrary/Client.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private ClientInterface _clientInterface;
+
+    private void Start()
+    {
+        _clientInterface = new ClientInterface();
+        _clientInterface.Start();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _clientInterface.SendInput(0);
+        if (Input.GetKey(KeyCode.S))
+            _clientInterface.SendInput(1);
+        if (Input.GetKey(KeyCode.A))
+            _clientInterface.SendInput(2);
+        if (Input.GetKey(KeyCode.D))
+            _clientInterface.SendInput(3);
+    }
+}
---
+++ TCPLibrary/Packet.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+struct InputPacket
+{
+    [MarshalAs(UnmanagedType.I4)]
+    public int typeOfService;
+    [MarshalAs(UnmanagedType.I4)]
+    public int displayId;
+    [MarshalAs(UnmanagedType.I4)]
+    public int payloadLength;
+    [MarshalAs(UnmanagedType.I4)]
+    public int input;
+}
+
+public class PacketManager
+{
+    private InputPacket _inputPacket = new InputPacket();
+
+    private int _displayId;
+
+    public PacketManager(int id)
+    {
+        _displayId = id;
+    }
+
+    public byte[] GetInputPacket(int input)
+    {
+        _inputPacket.typeOfService = 0;
+        _inputPacket.displayId = _displayId;
+        _inputPacket.payloadLength = 4;
+        _inputPacket.input = input;
+
+        return Serialize(_inputPacket);
+    }
+
+    private byte[] Serialize<T>(T packet)
+    {
+        var buffer = new byte[Marshal.SizeOf(typeof(T))];
+
+        var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        var pBuffer = gch.AddrOfPinnedObject();
+
+        Marshal.StructureToPtr(packet, pBuffer, false);
+        gch.Free();
+
+        return buffer;
+    }
+}
